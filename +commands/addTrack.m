4098d6993bac7c2f9652f9b0105e29e1b52fa935
John <*>
2014-07-22 20:44:56 -0400

    addtrack variable input param number fixed

--- +commands/addTrack.m
+++ +commands/addTrack.m
@@ -1,4 +1,4 @@
-function addTrack(v, filename, trackSpacing, fiber_len)	
+function addTrack(v, filename, varargin)	
 %MATcro('addTrack','dti.trk')
 %MATcro('addTrack','dti.trk', 100)
 %MATcro('addTrack','dti.trk', 50, 10)
@@ -7,5 +7,12 @@ function addTrack(v, filename, trackSpacing, fiber_len)
 % 2) fiber sampling space -1/x tracks will be sampled (default 1)
 % 3) minimun fiber length (default 5)
 	if (nargin < 2), return; end;
-	fileUtils.trk.addTrack(v, filename, trackSpacing, fiber_len);
+	inputs = cell2mat(varargin);
+    if(length(inputs) == 1)
+	  fileUtils.trk.addTrack(v, filename, inputs(1));
+	elseif(length(inputs) == 2)
+	  fileUtils.trk.addTrack(v, filename, inputs(1), inputs(2));
+	else
+	  fileUtils.trk.addTrack(v, filename);
+    end
 %end addTrack()
