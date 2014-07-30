8931e30840046153edf2a6addf137b515667c1e3
John <*>
2014-07-30 17:29:38 -0400

    gui data sent, input dlg

--- +gui/AddTracks_Callback.m
+++ +gui/AddTracks_Callback.m
@@ -8,8 +8,8 @@ v=guidata(obj);
     if isempty(track_filename), disp('load tracks cancelled'); return; end;
     track_filename=[track_pathname track_filename];
     prompt = {'Track Sampling (1/ts tracks will be loaded, increases speed but decreases information):','Minimum fiber length (only sampled tracks with this minimum fiber length will be rendered, increases speed but decreases information):'};
-	ans = inputdlg(prompt);
+	ans = inputdlg(prompt, 'Track Options', 1, {num2str(100), num2str(5)});
 	trackSpacing = str2double(ans(1));
 	fiberLen = str2double(ans(2));
-    fileUtils.trk.addTrack(obj,track_filename, trackSpacing, fiberLen);
+    fileUtils.trk.addTrack(v,track_filename, trackSpacing, fiberLen);
 %end Add_tracks_Callback()
