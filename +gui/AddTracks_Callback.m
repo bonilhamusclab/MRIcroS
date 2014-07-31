1d4e54e5103d88709a3b43074123aeacd1324b1c
John <*>
2014-07-31 13:48:46 -0400

    safely cancel load if user closes dialog

--- +gui/AddTracks_Callback.m
+++ +gui/AddTracks_Callback.m
@@ -8,6 +8,7 @@ function AddTracks_Callback(obj, eventdata)
     track_filename=[track_pathname track_filename];
     prompt = {'Track Sampling (1/ts tracks will be loaded, increases speed but decreases information):','Minimum fiber length (only sampled tracks with this minimum fiber length will be rendered, increases speed but decreases information):'};
 	ans = inputdlg(prompt, 'Track Options', 1, {num2str(100), num2str(5)});
+    if isempty(ans), disp('load cancelled'); return; end;
 	trackSpacing = str2double(ans(1));
 	fiberLen = str2double(ans(2));
 	v = guidata(obj);
