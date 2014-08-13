ba8dff0b422d38d781fcdc171d9bca321d09fc54
John <*>
2014-08-13 12:45:46 -0400

    removed debug output

--- +gui/CloseTracks_Callback.m
+++ +gui/CloseTracks_Callback.m
@@ -3,7 +3,6 @@ v = guidata(h);
 hasTracks = isfield(v, 'tracks');
 if(hasTracks)
 	tracks = v.tracks;
-	tracks
 	arrayfun(@(t)(delete(t.fibers)), tracks);
 	v = rmfield(v, 'tracks');
 	guidata(v.hMainFigure, v);
