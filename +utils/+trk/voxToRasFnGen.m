12640ec260a969b7728e36f4128186da54eb04dd
John <*>
2014-08-12 21:46:29 -0400

    issue warning message when matrices dont match

--- +utils/+trk/voxToRasFnGen.m
+++ +utils/+trk/voxToRasFnGen.m
@@ -11,7 +11,16 @@ function voxToRasFn = voxToRasFnGen(header)
 	voxelOrder = strcat(header.voxel_order, ''); %facilate processing
 
 	notMatched = find(affOrnt ~= voxelOrder);
+
+	if(sum(notMatched))
+		issueWarning(affOrnt, voxelOrder);
+	end
 	
 	putInAffSpace = @(x)(utils.trk.flipAxes(x, notMatched));
 
 	voxToRasFn = @(x)(affine*[putInAffSpace(x); ones(1, size(x,2))]);
+
+function issueWarning(affOrnt, voxelOrder)
+	warning(['afffine orientation (%s), and ',...
+		'voxel order (%s) do no match. Using voxel order'], ...
+		affOrnt, voxelOrder);
