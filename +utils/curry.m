ff7e50217f7b8c0b10836ca53aac564bae705d50
John <*>
2014-09-17 12:57:26 -0400

    spelling typo fix

--- +utils/curry.m
+++ +utils/curry.m
@@ -1,10 +1,10 @@
 function curriedFn = curry(fn, varargin)
 %function curriedFn = curry(fn, varargin)
-%   Origina Code Provided by Adam Bard in project: functools-for-matlab
+%   Original Code Provided by Adam Bard in project: functools-for-matlab
 %   https://github.com/adambard/functools-for-matlab/blob/master/%2Bfunctools/partial.m
 %   fn = @(a,b) a^2 + b;
-%   cFn = utils.curry(fn, 6);
-%   square6(3) %39
+%   square6ThanAddB = utils.curry(fn, 6);
+%   square6ThanAddB(3) %39
     alwaysProvided = varargin;
     curriedFn = @(varargin) fn(alwaysProvided{:}, varargin{:});
 end
