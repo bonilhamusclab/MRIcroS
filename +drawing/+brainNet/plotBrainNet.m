07ed3f666356bbf14b09ae162c88be6d0bbe2391
John <*>
2014-09-18 12:29:08 -0400

    vectorize plot brain net

--- +drawing/+brainNet/plotBrainNet.m
+++ +drawing/+brainNet/plotBrainNet.m
@@ -2,7 +2,14 @@
 %http://www.plosone.org/article/info%3Adoi%2F10.1371%2Fjournal.pone.0068910
 function [renderedNodes, renderedEdges] = plotBrainNet(nodes, edges)
 %function [renderedNodes, renderedEdges] = plotBrainNet(nodes, edges)
-[xSph, ySph, zSph] = sphere(100);
+    tic
+[xSph, ySph, zSph] = sphere(20);
+
+
+xCs = nodes(:,1);
+yCs = nodes(:,2);
+zCs = nodes(:,3);
+rads = nodes(:,5);
 
 numNodes = size(nodes, 1);
 
@@ -10,32 +17,32 @@ renderedNodes = zeros(1, numNodes);
 
 for i = 1:numNodes
     hold on
-    nodeInfo = nodes(i, :);
-    xC= nodeInfo(1);
-    yC= nodeInfo(2);
-    zC= nodeInfo(3);
-    rad = nodeInfo(5);
-    x = xSph.*rad + xC;
-    y = ySph.*rad + yC;
-    z = zSph.*rad + zC;
+    x = xSph.*rads(i) + xCs(i);
+    y = ySph.*rads(i) + yCs(i);
+    z = zSph.*rads(i) + zCs(i);
     renderedNodes(i) = mesh(x,y,z,'EdgeColor','red');
 end
 
-renderedEdges = zeros(numNodes);
-for r = 1:numNodes
-    %edges specified b/w nodes in triu matrix
-    %assuming no edge b/w node and self
-    firstColAfterDiag = r + 1;
-    for c = firstColAfterDiag:numNodes
-        hold on
-        if(edges(r, c))
-            x = [nodes(r, 1) nodes(c, 1)];
-            y = [nodes(r, 2) nodes(c, 2)];
-            z = [nodes(r, 3) nodes(c, 3)];
-            renderedEdges(r,c) = plot3(x,y,z);
-            renderedEdges(c,r) = renderedEdges(r,c);
-        end
-    end
-end
+edgesBinary = edges ~= 0 & ~tril(edges);
 
+xStarts = bsxfun(@times, edgesBinary, xCs);
+xStops = bsxfun(@times, edgesBinary, xCs');
+yStarts = bsxfun(@times, edgesBinary, yCs);
+yStops = bsxfun(@times, edgesBinary, yCs');
+zStarts = bsxfun(@times, edgesBinary, zCs);
+zStops = bsxfun(@times, edgesBinary, zCs');
+
+edgeIdxs = find(edgesBinary);
+numEdges = length(edgeIdxs);
+
+renderedEdges = zeros(numNodes);
+for i = 1:numEdges
+    edgeIdx = edgeIdxs(i);
+    hold on
+    x = [xStarts(edgeIdx) xStops(edgeIdx)];
+    y = [yStarts(edgeIdx) yStops(edgeIdx)];
+    z = [zStarts(edgeIdx) zStops(edgeIdx)];
+    renderedEdges(edgeIdx) = plot3(x,y,z);
+end
+    toc
 end
