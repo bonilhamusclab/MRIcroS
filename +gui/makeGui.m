a7d6ed867df6857bd569f8b4d67b400310475dbb
John <*>
2014-09-23 13:41:47 -0400

    change GUI to show correct name

--- +gui/makeGui.m
+++ +gui/makeGui.m
@@ -50,5 +50,6 @@ v.vprefs.az = 45; %camera azimuth
 v.vprefs.el = 10; %camera elevation
 guidata(v.hMainFigure,v);%store settings
 vFig = v.hMainFigure;
+set(vFig,'name','MRIcroS');
 drawing.redrawSurface(v);
 %end makeGUI()
