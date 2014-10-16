3049c6edbb9975fa372ca05668ba025be1d9eaf3
John <*>
2014-10-16 16:50:41 -0400

    typo fix

--- +utils/magnitudesToColors.m
+++ +utils/magnitudesToColors.m
@@ -1,6 +1,7 @@
-function colors = magnitudeToColor(magnitudes, clrMap)
-%function colors = magnitudeToColor(magnitudes, clrMap)
+function colors = magnitudesToColors(magnitudes, clrMap)
+%function colors = magnitudesToColors(magnitudes, clrMap)
 %convert Mx1 magnitudes into RGB Mx3 colors using desired colormap
+%note: expcets magnitudes to be from 0 to 1
 resolution = 64;
 
 clrMap = str2func(clrMap);
@@ -8,4 +9,4 @@ clr = clrMap(resolution);
 
 colors = ind2rgb(round(magnitudes(:) * 63)+1 ,clr);
 colors= reshape(colors,numel(magnitudes),3);
-%end magnitudeToColor()
+%end magnitudesToColors()
