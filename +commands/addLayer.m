4ad357bf959d0838a3cd8aeed87e5bf2dfc7444e
John <*>
2014-10-16 14:33:42 -0400

    project vol onto nifti surface by default

--- +commands/addLayer.m
+++ +commands/addLayer.m
@@ -13,7 +13,16 @@ function addLayer(v,filename,varargin)
 %   thresh (optional)
 %       * applies only to volumes (NiFTI)
 %       * Inf for midrange, -Inf for Otsu
-%       * defaults to Inf (midrange
+%       * defaults to Inf (midrange)
+%   projectVolumeColorMap (optional)
+%       * applies only to volumes (NifTI)
+%       * Is the same as redering volume as a surface and than projecting
+%       that volume onto the surface with projectVolume with the colorMap
+%       * set to 0 to turn off and render NifTI as a regular with no
+%       projections of the volume data
+%       * can be any of the colorMaps specified by Matlab
+%       ('jet','hsv','hot', etc...)
+%       * default jet
 %
 %   defaults are specified if empty string ('') is input for value, or if
 %   not specified
@@ -26,8 +35,8 @@ function addLayer(v,filename,varargin)
 %MRIcroS('addLayer','attention.nii.gz','','',-Inf);
 %MRIcroS('addLayer','attention.nii.gz',0.05,0,3); %threshold >3
 
-defaults = {.25, Inf, 0, 0};
-[reduce, thresh, smooth] = ...
+defaults = {.25, Inf, 0, 'jet'};
+[reduce, thresh, smooth, projectVolumeColorMap] = ...
     utils.parseInputs(varargin, defaults);
 
 reduceMesh = 1;
@@ -44,10 +53,10 @@ if exist(filename, 'file') == 0
 end;
 
 isBackground = v.vprefs.demoObjects;
-addLayerSub(v, isBackground, filename, reduce, reduceMesh, smooth, thresh);
+addLayerSub(v, isBackground, filename, reduce, reduceMesh, smooth, thresh, projectVolumeColorMap);
 %end addLayer()
 
-function addLayerSub(v, isBackground,  filename, reduce, reduceMesh, smooth, thresh)
+function addLayerSub(v, isBackground,  filename, reduce, reduceMesh, smooth, thresh, projectVolumeColorMap)
 %function addSurface(v, isBackground, readFileFn, filename, reduce, smooth, thresh)
 % filename: pial, nv, nii, nii.gz, vtk, gii image to open
 % reduce: 
@@ -64,7 +73,13 @@ if fileUtils.isMesh(filename)
     [v.surface(layer).faces, v.surface(layer).vertices, v.surface(layer).vertexColors] = fileUtils.readMesh(filename, reduceMesh);
 else    
     [v.surface(layer).faces, v.surface(layer).vertices] = fileUtils.readVox (filename, reduce, smooth, thresh);
-    v.surface(layer).vertexColors = [];
+    vertexColors = [];
+    if projectVolumeColorMap
+        vertices = v.surface(layer).vertices;
+        faces = v.surface(layer).faces;
+        vertexColors = utils.volumeIntensitiesToVertexColors(vertices, faces, filename,'','', projectVolumeColorMap);
+    end
+    v.surface(layer).vertexColors = vertexColors;
 end
 
 v.vprefs.demoObjects = false;
--- +commands/projectVolume.m
+++ +commands/projectVolume.m
@@ -1,81 +1,16 @@
 function projectVolume(v, surfaceIndex, volumeFile, varargin)
 %function projectVolume(v, surfaceIndex, volumeFile, varargin)
-% surfaceIndex: index of surface to project on
-% volumeFile: file name of nifti image to project onto surface
-% averageIntensities: set to 1 to average intensities
-%   default 0 for performance
-% interpolationMethod: 'nearest', 'linear', 'spline', 'cubic', (same as function
-% interp3)
-%   default: linear
-% colorMap: can use any of Matlabs color map schemes
-%   default: jet
-% applyGaussian: set to 1 if you want to gaussian smooth voxels before
-% projection
-%   default: 0
-%kernelSize: convolution kernel size for gaussian interpolation
-%   default: 13
-%standardDev:
-%   default: 2.53
-
-defaults = {0, 'linear', 'jet', 0, 13, 2.53};
-[averageIntensities, interpolationMethod, colorMap, applyGaussian, kernelSize, standardDev] = ...
-    utils.parseInputs(varargin, defaults);
-
-[hdr, voxels] = fileUtils.nifti.readNifti(volumeFile);
-
-voxels(isnan(voxels)) = 0; %is this the right way to treat NaNs?
-
-if(applyGaussian)
-    voxels = smooth3(voxels,'gaussian', kernelSize, standardDev);
-end
+%a wrapper function for utils.projectVolume
+%faces and vertices obtained from surface index, than all inputs passed to
+%utils.volumeIntensitiesToVertexColors
+%help utils.volumeIntensitiesToVertexColors
 
 surface = v.surface(surfaceIndex);
 faces = surface.faces;
 vertices = surface.vertices;
 
-verticesRasSpace = [vertices ones(size(vertices,1),1)]';
-verticesVoxSpace = hdr.mat\verticesRasSpace;
-
-%not sure why this must 2,1,3
-surfaceIntensities = interp3(voxels, verticesVoxSpace(2,:), verticesVoxSpace(1,:), verticesVoxSpace(3,:), interpolationMethod);
-    
-
-if averageIntensities % vertex intensity smoothing: average intensity of all connected vertices: BrainNet.m EC.vol.mapalgorithm=2
-    fprintf('Averaging across %d vertices - this may be very slow\n', numel(surfaceIntensities));
-    clrTmp = surfaceIntensities; %original estimates for each vertex color
-    for i = 1:numel(surfaceIntensities)
-        [m,~] = find(faces == i);
-        u = unique(faces(m,:));
-        surfaceIntensities(i) = mean(clrTmp(u));
-    end
-end
-
-range = max(surfaceIntensities) - min(surfaceIntensities);
-if range ~= 0 %normalize for range 0 (black) to 1 (white)
-    surfaceIntensities = (surfaceIntensities - min(surfaceIntensities)) / range;
-    %next- color balance, so typical voxels are mid-gray
-        %we could use a histogram
-        % http://angeljohnsy.blogspot.com/2011/04/matlab-code-histogram-equalization.html?m=1
-        %instead, since range is 0..1 we will use power function to make median = 0.5
-        % to determine power exponent, we compute Logarithm to an arbitrary base http://en.wikipedia.org/wiki/Logarithm 
-        mdn = median(surfaceIntensities(:));
-        pow = log(0.5)/log(mdn);
-        surfaceIntensities = power(surfaceIntensities, pow);
-        vertexColors = intensityToColorSub(surfaceIntensities, colorMap);
-end
+vertexColors = utils.volumeIntensitiesToVertexColors(vertices, faces, volumeFile, varargin);
 
 v.surface(surfaceIndex).vertexColors = vertexColors;
 
 drawing.redrawSurface(v);
-
-
-function colors = intensityToColorSub(intensities, clrMap)
-%convert scalar Mx1 image into RGB Mx3 image using desired colormap
-resolution = 64;
-
-clrMap = str2func(clrMap);
-clr = clrMap(resolution);
-
-colors = ind2rgb(round(intensities(:) * 63)+1 ,clr);
-colors= reshape(colors,numel(intensities),3);
-%end colorSub()
