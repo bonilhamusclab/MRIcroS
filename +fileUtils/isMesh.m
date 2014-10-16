c5414de51e1767d0c9e7f6e2ebfe94edf7f80029
John <*>
2014-10-16 12:15:56 -0400

    comment fix

--- +fileUtils/isMesh.m
+++ +fileUtils/isMesh.m
@@ -1,5 +1,5 @@
 function is = isMesh(filename)
-%function is = isPial(filename)
+%function is = isMesh(filename)
 if fileUtils.isTrib(filename) || fileUtils.isVtk(filename) || fileUtils.isPly(filename) || fileUtils.isPial(filename) || fileUtils.isNv(filename) || fileUtils.isGifti(filename) 
     is = true;
 else
