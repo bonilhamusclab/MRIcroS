748f664a80a0a26bdc1d2ec4d649a682d8833b35
John <*>
2014-10-17 17:48:32 -0400

    update comments

--- +commands/hideWireframe.m
+++ +commands/hideWireframe.m
@@ -1,8 +1,6 @@
 function hideWireframe(v, layer)
-% inputs: layerNumber
-% note: rgb can not be a string such as 'blue', must be an array
-%MRIcroS('layerRGBA', 1, [1 0, 0]) %set layer 1 to red
-%MRIcroS('layerRGBA', 1:2, [1 0, 0]) %set layer 1 & 2 to red
+%function hideWireframe(v, layer)
+% inputs: layer - layerNumber to remove wireframe from
 
 v.vprefs.showEdges(layer) = 0;
 
