9a4793686d212cc081b6ad0ed6671796f47a670c
John <*>
2014-10-20 13:45:33 -0400

    addTrack uses input validation

--- +commands/addTrack.m
+++ +commands/addTrack.m
@@ -4,8 +4,9 @@ function addTrack(v, filename, varargin)
 %MRIcroS('addTrack','dti.trk', 50, 10)
 %inputs: 
 % 1) filename
-% 2) fiber sampling space: 1/x tracks will be sampled (default 1)
-% 3) minimun fiber length (default 5)
+%   Optional:
+% 2) trackSpacing: 1/x tracks will be sampled (default 100)
+% 3) fiberLengthThreshold: minimun fiber length before thresholding (default 5)
 % --- Load trackvis fibers http://www.trackvis.org/docs/?subsect=fileformat
 
 [filename, isFound] = fileUtils.isFileFound(v, filename);
@@ -13,14 +14,15 @@ if ~isFound
     fprintf('Unable to find "%s"\n',filename); 
     return; 
 end;
-if (nargin < 2), return; end;
-trackSpacing = 100;
-fiber_len = 5;
-if (length(varargin) >= 1) && isnumeric(varargin{1}), trackSpacing = varargin{1}; end;
-if (length(varargin) >= 2) && isnumeric(varargin{2}), fiber_len = varargin{2}; end;
+
+inputs = parseInputParamsSub(varargin);
+
+trackSpacing = inputs.trackSpacing;
+fiberLen = inputs.fiberLengthThreshold;
+
 hold on
 [header,data] = fileUtils.trk.readTrack(filename);
-renderedFibers = drawing.trk.plotTrack(header, data, trackSpacing, fiber_len);
+renderedFibers = drawing.trk.plotTrack(header, data, trackSpacing, fiberLen);
 
 hasTrack = isfield(v,'tracks');
 tracksIndex = 1;
@@ -30,3 +32,16 @@ guidata(v.hMainFigure, v);
 drawing.removeDemoObjects(v);
 
 %end addTrackSub()
+
+function inputParams = parseInputParamsSub(args)
+p = inputParser;
+d.fiberLengthThreshold = 5; d.trackSpacing = 100;
+
+p.addOptional('trackSpacing',1, ...
+    @(x) validateattributes(x, {'numeric'},{'positive','real','integer'}));
+p.addOptional('fiberLengthThreshold',5, ...
+    @(x) validateattributes(x, {'numeric'},{'nonnegative', 'real'}));
+
+p = utils.stringSafeParse(p, args, fieldnames(d), d.fiberLengthThreshold, d.trackSpacing);
+
+inputParams = p.Results;
