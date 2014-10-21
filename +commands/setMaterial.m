3e36adf0731247eae3416247293ff3bc69ab7e67
John <*>
2014-10-21 13:50:49 -0400

    setMaterial uses input validation

--- +commands/setMaterial.m
+++ +commands/setMaterial.m
@@ -1,13 +1,40 @@
 function setMaterial(v,varargin)
+%function setMaterial(v,varargin)
+% --- set surface appearance (shiny, matte, etc)
 % inputs: ambient(0..1), diffuse(0..1), specular(0..1), specularExponent(0..inf), backFaceLighting (0 or 1)
 %MRIcroS('setMaterial', 0.5, 0.5, 0.7, 100, 1);
-% --- set surface appearance (shiny, matte, etc)
+%if not specified, will use current value
+
 if (length(varargin) < 1), return; end;
-if (length(varargin) >= 1) && isnumeric(varargin{1}), v.vprefs.materialKaKdKsn(1) = varargin{1}; end;
-if (length(varargin) >= 2) && isnumeric(varargin{2}), v.vprefs.materialKaKdKsn(2) = varargin{2}; end;
-if (length(varargin) >= 3) && isnumeric(varargin{3}), v.vprefs.materialKaKdKsn(3) = varargin{3}; end;
-if (length(varargin) >= 4) && isnumeric(varargin{4}), v.vprefs.materialKaKdKsn(4) = varargin{4};end;
-if (length(varargin) >= 5) && isnumeric(varargin{5}), v.vprefs.backFaceLighting = varargin{5};end;
+
+inputResults = parseInputParamsSub(v.vprefs.materialKaKdKsn, v.vprefs.backFaceLighting, varargin);
+
+v.vprefs.materialKaKdKsn(1) = inputResults.ambient;
+v.vprefs.materialKaKdKsn(2) = inputResults.diffuse;
+v.vprefs.materialKaKdKsn(3) = inputResults.specular;
+v.vprefs.materialKaKdKsn(4) = inputResults.specularExponent;
+v.vprefs.backFaceLighting = inputResults.backFaceLighting;
+
 guidata(v.hMainFigure,v);%store settings
 drawing.redrawSurface(v);
 %end setMaterial()
+
+
+function inputParams = parseInputParamsSub(materialKaKdKsn, backFaceLighting, args)
+
+d.a = materialKaKdKsn(1);
+d.d = materialKaKdKsn(2);
+d.s = materialKaKdKsn(3);
+d.n = materialKaKdKsn(4);
+d.backFaceLighting = backFaceLighting;
+
+p = inputParser;
+p.addOptional('ambient',d.a, @(x) validateattributes(x, {'numeric'}, {'>=', 0, '<=' 1}));
+p.addOptional('diffuse',d.d, @(x) validateattributes(x, {'numeric'}, {'>=', 0, '<=', 1}));
+p.addOptional('specular',d.s, @(x) validateattributes(x, {'numeric'}, {'>=', 0, '<=', 1}));
+p.addOptional('specularExponent', d.n, @(x) validateattributes(x, {'numeric'}, {'nonnegative'}));
+p.addOptional('backFaceLighting', d.backFaceLighting, @(x) validateattributes(x, {'numeric'}, {'binary'}));
+
+p = utils.stringSafeParse(p, args, fieldnames(d), d.a, d.d, d.s, d.n, d.backFaceLighting);
+
+inputParams = p.Results;
