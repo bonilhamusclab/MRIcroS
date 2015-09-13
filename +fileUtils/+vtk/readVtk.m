7c35357f2b941f4910d37541ee9869bd275e11fa
Chris Rorden <rorden@mailbox.sc.edu>
2015-09-13 16:51:47 -0400

    Binary STL files for Slicer/Mango compatibility

--- +fileUtils/+vtk/readVtk.m
+++ +fileUtils/+vtk/readVtk.m
@@ -4,38 +4,95 @@ function [vertex,face] = readVtk(filename)
 %   'face' is a 'nb.face x 3' array specifying the connectivity of the mesh.
 %   Copyright (c) Mario Richtsfeld, distributed under BSD license
 % http://www.mathworks.com/matlabcentral/fileexchange/5355-toolbox-graph/content/toolbox_graph/read_vtk.m
+% http://www.ifb.ethz.ch/education/statisticalphysics/file-formats.pdf
+% ftp://ftp.tuwien.ac.at/visual/vtk/www/FileFormats.pdf
+%The VTK format supports a wide range of datasets, Chris Rorden modified
+% this version to read some binary images and provide sensible error reporting 
+% for unsupported variants of the VTK format
 % --- read VTK format mesh
 fid = fopen(filename,'r');
 if( fid==-1 )
     error('Can''t open the VTK file %s',filename);
 end
-str = fgets(fid);   % -1 if eof
+str = fgets(fid);   % -1 if eof, signature, e.g. "# vtk DataFile Version 3.0"
 if ~strcmp(str(3:5), 'vtk')
     error('The file is not a valid VTK one.');    
 end
 % read header
-str = fgets(fid);
-str = fgets(fid);
-str = fgets(fid);
-str = fgets(fid);
-nvert = sscanf(str,'%*s %d %*s', 1);
-% read vertices
-[A,cnt] = fscanf(fid,'%f %f %f', 3*nvert);
-if cnt~=3*nvert
-    warning('Problem in reading vertices.');
+str = fgets(fid); % notes, e.g. "vtk output ImageThreshold=53.0" 
+formatStr = fgets(fid); % datatype, "BINARY" or "ASCII" 
+isBinary = false;
+if strcmpi(formatStr(1:6), 'BINARY')
+    isBinary = true;
+elseif ~strcmpi(formatStr(1:5), 'ASCII')
+    error('Only able to read VTK images saved as BINARY or ASCII, not %s', formatStr);
 end
-A = reshape(A, 3, cnt/3);
-vertex = A;
+kindStr = fgets(fid); % kind, e.g. "DATASET POLYDATA" or "DATASET STRUCTURED_ POINTS"
+if isempty(strfind(upper(kindStr),'POLYDATA'))
+    error('Only able to read VTK images saved as POLYDATA, not %s', kindStr);
+end
+vertStr = fgets(fid); % number of vertices, e.g. "POINTS 685462 float"
+if isempty(strfind(upper(vertStr),'POINTS'))
+    error('Expected header to report "POINTS", not %s', vertStr);
+end
+nvert = sscanf(vertStr,'%*s %d %*s', 1);
+% read vertices
+if isBinary
+    cnt = 3*nvert;
+    vtx = fread(fid, cnt, 'float32=>float32');
+else
+    [A,cnt] = fscanf(fid,'%f %f %f', 3*nvert);
+    str = fgets(fid); %read EOLN for vertices
+    if cnt~=3*nvert
+        warning('Problem in reading vertices.');
+    end
+    A = reshape(A, 3, cnt/3);
+    vertex = A;
+end;
 % read polygons
-str = fgets(fid);
-str = fgets(fid);
+str = fgets(fid); %e.g. "POLYGONS 6 30" 
 info = sscanf(str,'%c %*s %*s', 1);
+if isBinary
+    if(info ~= 'P')
+        error('Only able to read binary VTK files with POLYGONS: %s', str); 
+    end
+    nface = sscanf(str,'%*s %d %*s', 1);
+    cnt = sscanf(str,'%*s %*s %d', 1);
+    if (cnt ~= (4 * nface))
+       error('Only able to read VTK files with triangles: %s', str);
+    end
+    A = fread(fid, cnt, 'uint32=>uint32');
+    if A(1) ~= 3 %not native endian!!!!
+        %A = swapbytes( uint32(A));
+        A = swapbytes(A);
+        vtx = swapbytes(vtx);
+    end
+    if A(1) ~= 3 %not native endian!!!!
+        error('This vtk file is borked');
+    end
+    A = reshape(A, 4, cnt/4);
+    face = double(A(2:4,:)+1);
+    
+    vertex = double(vtx);
+    vertex = reshape(vertex, 3, numel(vertex)/3);
+    fclose(fid);
+    %vertex %report vertices
+    %face'-1 %report faces
+    return;
+    
+end
+
+
 if((info ~= 'P') && (info ~= 'V'))
     str = fgets(fid);    
     info = sscanf(str,'%c %*s %*s', 1);
 end
 if(info == 'P')
-        nface = sscanf(str,'%*s %d %*s', 1);
+    nface = sscanf(str,'%*s %d %*s', 1);
+    cnt = sscanf(str,'%*s %*s %d', 1);
+    if (cnt ~= (4 * nface))
+       error('Only able to read VTK files with triangles: %s', str);
+    end
     [A,cnt] = fscanf(fid,'%d %d %d %d\n', 4*nface);
     if cnt~=4*nface
         warning('Problem in reading faces.');
@@ -60,4 +117,6 @@ if((info ~= 'P') && (info ~= 'V'))
     face = 0;
 end
 fclose(fid);
+%vertex %report vertices
+%face'-1 %report faces
 %end readVtk()
--- +fileUtils/+vtk/writeVtk.m
+++ +fileUtils/+vtk/writeVtk.m
@@ -4,6 +4,10 @@ function writeVtk(vertex,face,filename)
 [nF nFd] =size(face);
 [nV nVd] =size(vertex);
 if (nF <1) || (nV <3 || (nFd ~=3) || (nVd ~=3)), warning('Problem with writeVtk'); return; end; 
+if nF > 1024 %binary takes less disk space - use for complex images.... 
+    writeBinaryVtkSub(vertex,face,filename);
+    return;
+end
 fid = fopen(filename, 'wt');
 fprintf(fid, '# vtk DataFile Version 3.0\n');
 fprintf(fid, 'Comment: created with MRIcroS\n');
@@ -15,3 +19,23 @@ fprintf(fid, 'POLYGONS %d %d\n',nF, nF*(nFd+1));
 fprintf(fid, '3 %d %d %d\n', (face-1)');
 fclose(fid);
 %end writeVtk()
+
+function writeBinaryVtkSub(vertex,face,filename)
+%write binary vtk file - we save as BIG ENDIAN since Mango prefers this
+% also checked compatibility with Slicer
+[nF nFd] =size(face);
+[nV nVd] =size(vertex);
+if (nF <1) || (nV <3 || (nFd ~=3) || (nVd ~=3)), warning('Problem with writeVtk'); return; end; 
+fid = fopen(filename, 'wb');
+fprintf(fid, '# vtk DataFile Version 3.0\n');
+fprintf(fid, 'Comment: created with MRIcroS\n');
+fprintf(fid, 'BINARY\n');
+fprintf(fid, 'DATASET POLYDATA\n');
+fprintf(fid, 'POINTS %d float\n',nV);
+fwrite(fid, vertex', 'float32', 'ieee-be' );
+fprintf(fid, 'POLYGONS %d %d\n',nF, nF*(nFd+1));
+f3(1:nF) = 3;
+fc = [f3' (face - 1)];
+fwrite(fid, fc', 'uint32', 'ieee-be' );
+fclose(fid);
+%end writeBinaryVtkSub()
