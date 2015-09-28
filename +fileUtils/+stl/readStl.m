4632bc5c4436574607ff6b74b3ff6facc88e69c7
Chris Rorden <rorden@mailbox.sc.edu>
2015-09-27 20:19:52 -0400

    Improved STL visualization
    remove redundant vertices

--- +fileUtils/+stl/readStl.m
+++ +fileUtils/+stl/readStl.m
@@ -40,5 +40,40 @@ vertices = reshape(vertices',3,(3 * num_tri))';
 faces = 1: (3 * num_tri);
 faces = reshape(faces,3, num_tri)';
 fclose(fid);
+%WARNING STL does not record redundancy in vertices. This uses needless
+%memory and hinders vertex smoothing. Lets reduce this: 
+% http://www.mathworks.com/matlabcentral/fileexchange/29986-patch-slim--patchslim-m-
+% Copyright (c) 2011, Francis Esmonde-White
+% All rights reserved.
+% 
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are
+% met:
+% 
+%     * Redistributions of source code must retain the above copyright
+%       notice, this list of conditions and the following disclaimer.
+%     * Redistributions in binary form must reproduce the above copyright
+%       notice, this list of conditions and the following disclaimer in
+%       the documentation and/or other materials provided with the distribution
+% 
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+% POSSIBILITY OF SUCH DAMAGE.
+[vnew, indexm, indexn] =  unique(vertices, 'rows'); %#ok<ASGLU>
+faces = indexn(faces);
+fprintf('Redundant vertices removed %d -> %d (%g)\n',  size(vertices,1), size(vnew,1), size(vnew,1)/size(vertices,1));
+vertices = vnew;
+
+
+
+
 %end readStl()
 
