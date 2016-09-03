16d015b94b5da008b37c0990afed4220df411583
Chris Rorden <rorden@mailbox.sc.edu>
2016-09-03 19:58:59 -0400

    fix affineOrientation

--- +utils/+trk/affineOrientation.m
+++ +utils/+trk/affineOrientation.m
@@ -1,51 +1,40 @@
-function label = affineOrientation( affMatrix )
-%function label = affineOrientation( affMatrix )
+function RASLPI = affineOrientation( affMatrix )
+%function RASLPI = affineOrientation( affMatrix )
 %   returns implied label (LPS, LAS, etc...)  based on affMatrix
-%   Code based on nibabel orientations 
+%Similar function to nibabel orientations
 %   https://github.com/nipy/nibabel/blob/master/nibabel/orientations.py
-    
-    [q, p] = size(affMatrix);
-    q = q - 1;
-    p = p - 1;
-    
-    rzs = affMatrix(1:q, 1:p);
-    
-    zooms = sqrt(sum(rzs.^2));
-    zooms(zooms == 0) = 1;
-    
-    rs = zeros(size(rzs));
-    rows = length(zooms);
-    for i =1: rows
-        rs(i,:) = rzs(i,:)./zooms;
-    end
-    
-    [P, S, Q] = svd(rs);
-    Qs = Q';
-    
-    R = P * Qs;
-    
-    orientation = zeros(p,2) * NaN;
-    for i =1:p
-        col = R(:,i);
-        if(sum(abs(col)) == 0)
-            return;
-        else
-            [~, maxRow] = max(abs(col));
-            orientation(i) = sign(col(maxRow));
-            R(maxRow, :) = 0;
-        end
-    end
-        
-    lpi = 'LPI';
-    ras = 'RAS';
-    label = '';
-    
-    for i = 1:length(orientation)
-        direction = orientation(i);
-        if(direction == -1)
-            label = strcat(label, lpi(i));
-        else
-            label = strcat(label, ras(i));
-        end
+%inspired by Xiangrui Li's dicm2nii
+
+R = affMatrix;
+if numel(R) >= 16
+    R = R(1:3,1:3); %convert 4x4 matrix to 3x3
+end
+foo = abs(R);
+[~, ixyz] = max(foo); % orientation info: perm of 1:3
+if ixyz(2) == ixyz(1), foo(ixyz(2),2) = 0; [~, ixyz(2)] = max(foo(:,2)); end
+%if any(ixyz(3) == ixyz(1:2)), ixyz(3) = setdiff(1:3, ixyz(1:2)); end %next line simpler
+ixyz(3) = 6 - ixyz(2) - ixyz(1); %make unique: regardless of permutation, [1 2 3] always equal 6
+%set sign
+for i = 1 : 3
+    [~, idx] = max(foo(:,i));
+    ixyz(i) = ixyz(i) * sign(R(idx,i));
+end
+%convert index [-1 2 3] to TrackVis style, e.g. "LAS"
+RASLPI = 'XXX';
+for i = 1 : 3
+    switch ixyz(i)
+        case 1 
+            RASLPI(i) = 'R';
+        case -1 
+            RASLPI(i) = 'L';
+        case 2 
+            RASLPI(i) = 'A';
+        case -2 
+            RASLPI(i) = 'P';
+        case 3 
+            RASLPI(i) = 'S';
+        case -3 
+            RASLPI(i) = 'I';
     end
 end
+%end affineOrientationSub()
